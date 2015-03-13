589935ad3e2dd29a2206d7723c47e96b449c6b3b
pcrane <pcrane@goscoutgo.com>
2015-03-13 11:48:18 -0400

    Swap btc query to https.

--- static/app.js
+++ static/app.js
@@ -69,7 +69,7 @@ $(function(){
     var symbols = ['IBM',"BAC","AIZ","UHAL","AEG"];
 
     function get_data(symbol_csv) {
-        var url = 'http://query.yahooapis.com/v1/public/yql';
+        var url = 'https://query.yahooapis.com/v1/public/yql';
         var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol_csv + "')");
         $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env").done(function (data) {
             var compiled_template = _.template(price_template);
