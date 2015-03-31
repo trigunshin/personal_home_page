cc66eb045a9c6dd3ab1101d5cff158fdaae613b5
pcrane <pcrane@goscoutgo.com>
2015-03-31 13:38:39 -0400

    Updated email fetch/parse methods.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import email, imaplib
 from config import settings
+from dateutil import parser
 from flask import Flask, redirect, render_template
 from jinja2 import Markup
 from os import getenv
@@ -13,10 +14,34 @@ if email_data_string:
     results = [pair.split('||') for pair in email_data_string.split('|_|')]
 else:
     results = []
-print results
 
+email_headers = ['Subject', 'Date']
 
-def _fetch_mail(username, password):
+def _get_email_data(client, uid, email_headers):
+    ret = {}
+
+    result, data = client.uid('fetch', uid, '(RFC822)')
+    raw_email = data[0][1]
+    email_message = email.message_from_string(raw_email)
+
+    ret['to'] = email_message['To']
+     # for parsing "Yuji Tomita" <yuji@grovemade.com>
+    ret['from'] = email.utils.parseaddr(email_message['From'])
+
+    ret['headers'] = {}
+    for key, val in email_message.items():
+        if key in email_headers:
+            if key == 'Date':
+                ret[key] = parser.parse(val)
+            else:
+                ret[key] = val
+
+    # re-mark unread
+    client.uid('STORE', uid, '-FLAGS', '\SEEN')
+
+    return ret
+
+def _fetch_mail(username, password, email_headers):
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(username, password)
     mail.select("inbox")
@@ -26,18 +51,14 @@ def _fetch_mail(username, password):
         print result, data
         return None
 
-    latest_email_uid = data[0].split()[-1]
-    result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-    raw_email = data[0][1]
-    email_message = email.message_from_string(raw_email)
+    email_uids = data[0].split(' ')
+    email_data = [_get_email_data(mail, uid, email_headers) for uid in email_uids]
+    # latest_email_uid = email_uids[-1]
 
-    print email_message['To']
-    print email.utils.parseaddr(email_message['From']) # for parsing "Yuji Tomita" <yuji@grovemade.com>
-    print email_message.items() # print all headers
-    # re-mark unread
-    mail.uid('STORE', latest_email_uid, '-FLAGS', '\SEEN')
     mail.close()
     mail.logout()
+
+    return email_data
 
 
 @app.route('/gmail/imap')
